088d96bbb1c4b0c7bdb2771646e5bc730085985f
Neskj <pronin.qq@gmail.com>
2024-05-29 12:40:57 +0300

    connect data sourse

--- RepairJournal/src/main/java/neskj/RepairJournal/RepairJournalApplication.java
+++ RepairJournal/src/main/java/neskj/RepairJournal/RepairJournalApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RepairJournalApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RepairJournalApplication.class, args);
+
+		System.out.println("Hello RepairJournal !!!");
 	}
 
 }
