d65f1094ee04813bf47c33358be4b3c6ee0d71fe
Neskj <pronin.qq@gmail.com>
2024-05-29 12:30:57 +0300

    first commit

---
+++ RepairJournal/src/main/java/neskj/RepairJournal/RepairJournalApplication.java
@@ -0,0 +1,13 @@
+package neskj.RepairJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairJournalApplication.class, args);
+	}
+
+}
---
+++ RepairJournal/src/test/java/neskj/RepairJournal/RepairJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package neskj.RepairJournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
